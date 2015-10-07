c35571c78427ea57f22941bd4cb4f0c0d39c4ec3
docbronovan <brockdonovan@gmail.com>
2015-10-07 17:25:57 -0400

    first commit

---
+++ lib/mega_lotto.rb
@@ -0,0 +1,6 @@
+require "mega_lotto/version"
+require "mega_lotto/drawing"
+
+module MegaLotto
+  # Your code goes here...In general, you'll only want to put configuration and instantiation code in this file.
+end
